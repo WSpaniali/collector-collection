8acd3e111d3c4ec1bd5fc5fd1a5560b016f1b465
Wojciech Odrobina <wowikro@gmail.com>
2023-11-27 22:25:55 +0100

    Initial commit

---
+++ src/main/java/eu/clltr/collectorcollection/CollectorCollectionApplication.java
@@ -0,0 +1,13 @@
+package eu.clltr.collectorcollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectorCollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectorCollectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/clltr/collectorcollection/CollectorCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.clltr.collectorcollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectorCollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
